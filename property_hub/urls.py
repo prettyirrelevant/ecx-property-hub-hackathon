da9c010537ee2ba8582f3d066d1848f1b292d401
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-03 08:26:39 +0100

    changed index url to swagger

--- property_hub/urls.py
+++ property_hub/urls.py
@@ -5,7 +5,7 @@ from drf_yasg.views import get_schema_view
 
 
 def index(request):
-    return redirect("redoc/")
+    return redirect("swagger/")
 
 
 schema_view = get_schema_view(
@@ -21,6 +21,10 @@ urlpatterns = [
     path("", index, name="index"),
     path("api/", include("accounts.urls")),
     path("api/", include("listings.urls")),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger"),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger",
+    ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
